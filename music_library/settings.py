2e110604ad870d938e82cd47c5558af06c43b235
Djaypm89 <djaypm89@gmail.com>
2021-09-07 13:07:16 -0400

    re push

--- music_library/settings.py
+++ music_library/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o#46$)quiixur@$&vkgo@*kds7a%%p6!2%o5ki-$)xxwx+_9ta'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'musicapp',
 ]
 
 MIDDLEWARE = [
@@ -72,14 +73,6 @@ WSGI_APPLICATION = 'music_library.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -125,7 +118,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-        from music_library.local_settings import *
+    from music_library.local_settings import *
 except ImportError:
     pass
 
